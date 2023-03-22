02396ac07ba4260743f0af07c0cade2123f6e488
dohlee <dohlee.bioinfo@gmail.com>
2023-03-23 01:10:42 +0900

    0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
   name = 'igfold-pytorch',
   packages = find_packages(exclude=[]),
   include_package_data = True,
-  version = '0.0.1',
+  version = '0.0.2',
   license='MIT',
   description = 'IgFold - Pytorch',
   author = 'Dohoon Lee',
